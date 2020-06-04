c51f43a9dc3eaf0dea84bd5e12ff9b04475290f0
kansiree <kansiree_print@hotmail.com>
2020-06-04 22:40:25 +0700

    system api

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type Masterdata []struct {
+	CreatedDate string `json:"created_date"`
+	FullName    string `json:"full_name"`
+	ID          string `json:"id"`
+}
+type Masters []Masterdata
 
 func main() {
 	handleRequest()
@@ -20,21 +31,21 @@ func handleRequest() {
 	http.ListenAndServe(getPort(), nil)
 }
 
-type addressBook struct {
-	Firstname string
-	Lastname  string
-	Code      int
-	Phone     string
-}
-
 func getAddressBookAll(w http.ResponseWriter, r *http.Request) {
-	addBook := addressBook{
-		Firstname: "Chaiyarin",
-		Lastname:  "Niamsuwan",
-		Code:      1993,
-		Phone:     "0870940955",
+
+	var masters Masterdata
+	response, err := getJSON("SELECT * FROM t016ffukzsi0y5ie.master_system")
+
+	errpare := json.Unmarshal([]byte(response), &masters)
+	if errpare != nil {
+		log.Fatalln(errpare)
 	}
-	json.NewEncoder(w).Encode(addBook) // (1)
+	if err != nil {
+		fmt.Println(err)
+
+	} else {
+		json.NewEncoder(w).Encode(masters)
+	}
 }
 
 func getPort() string {
@@ -44,4 +55,50 @@ func getPort() string {
 		fmt.Println("No Port In Heroku " + port)
 	}
 	return ":" + port
+}
+
+func getJSON(sqlString string) (string, error) {
+	db, err := sql.Open("mysql", "sz0debklevf8wjhf:gu2af8swu50tjc3k@tcp(u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306)/t016ffukzsi0y5ie")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.Query(sqlString)
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+	columns, err := rows.Columns()
+	if err != nil {
+		return "", err
+	}
+
+	count := len(columns)
+	tableData := make([]map[string]interface{}, 0)
+	values := make([]interface{}, count)
+	valuePtrs := make([]interface{}, count)
+
+	for rows.Next() {
+		for i := 0; i < count; i++ {
+			valuePtrs[i] = &values[i]
+		}
+		rows.Scan(valuePtrs...)
+		entry := make(map[string]interface{})
+		for i, col := range columns {
+			var v interface{}
+			val := values[i]
+			b, ok := val.([]byte)
+			if ok {
+				v = string(b)
+			} else {
+				v = val
+			}
+			entry[col] = v
+		}
+		tableData = append(tableData, entry)
+	}
+	jsonData, err := json.Marshal(tableData)
+	if err != nil {
+		return "", err
+	}
+	return string(jsonData), nil
 }
